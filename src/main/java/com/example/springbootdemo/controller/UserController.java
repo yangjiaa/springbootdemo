51f77d190c9a2e96bf9b1b8f343903855a8e3b22
yangjiaa <yangjia@cnksi.com>
2019-07-03 17:42:33 +0800

    Revert "测试3"
    This reverts commit f13c08787cfd05f4046175222f48bda68ba9c917.
    
    # Conflicts:
    #	src/main/java/com/example/springbootdemo/controller/UserController.java

--- src/main/java/com/example/springbootdemo/controller/UserController.java
+++ src/main/java/com/example/springbootdemo/controller/UserController.java
@@ -47,7 +47,6 @@ public class UserController {
         userJPA.deleteById(id);
         System.currentTimeMillis();
         System.out.println("12");
-        System.out.println("13");
         System.out.println("14");
         return userJPA.findAll();
     }
