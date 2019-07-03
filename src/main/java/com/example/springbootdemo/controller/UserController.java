85d056e8df68d5ef44c97d8dd575f2ef39ed7a11
yangjiaa <yangjia@cnksi.com>
2019-07-03 18:04:04 +0800

    tt

--- src/main/java/com/example/springbootdemo/controller/UserController.java
+++ src/main/java/com/example/springbootdemo/controller/UserController.java
@@ -46,6 +46,7 @@ public class UserController {
     {
         userJPA.deleteById(id);
         System.currentTimeMillis();
+        System.currentTimeMillis();
         return userJPA.findAll();
     }
 
